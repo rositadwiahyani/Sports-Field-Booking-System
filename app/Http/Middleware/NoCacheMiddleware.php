faae95964b3e5704fa5796f440e3f8a0095eda77
FrostyJesse <timothydamanik8@gmail.com>
2026-05-31 23:53:32 +0700

    Fix NoCacheMiddleware headers error check

--- app/Http/Middleware/NoCacheMiddleware.php
+++ app/Http/Middleware/NoCacheMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class NoCacheMiddleware
 {
@@ -12,13 +13,15 @@ class NoCacheMiddleware
      * This ensures that after logout, pressing the browser "Back" button
      * will not show cached authenticated pages.
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
 
-        $response->headers->set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
-        $response->headers->set('Pragma', 'no-cache');
-        $response->headers->set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
+        if ($response instanceof Response) {
+            $response->headers->set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
+        }
 
         return $response;
     }
